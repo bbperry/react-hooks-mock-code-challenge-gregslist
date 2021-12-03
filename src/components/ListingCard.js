9e9433a22ff37e30c3d17553009b209ea6cd65b6
Ben <benbperry1@gmail.com>
2021-12-02 19:40:53 -0800

    commit tree f672f62fffbccbb258365219ed7d3e26c154fc2a

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ListingCard() {
+function ListingCard({listing}) {
+
+  const { id, description, image, location} = listing
+
+  const [isFavorite, setIsFavorite] = useState(false)
+
+  function clickFavorite() {
+    setIsFavorite(!isFavorite)
+  }
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={image} alt={description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {isFavorite ? (
+          <button className="emoji-button favorite active" onClick={clickFavorite}>★</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button className="emoji-button favorite" onClick={clickFavorite}>☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
+        <strong>{description}</strong>
+        <span> · {location}</span>
         <button className="emoji-button delete">🗑</button>
       </div>
     </li>
